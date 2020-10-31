d316093ef6fd2964cf4305aaf5d2c95b2bbbb685
Aaron Walsman <aaronwalsman@gmail.com>
2020-10-31 15:56:46 -0700

    now just a function

--- brick_gym/datasets/list_paths.py
+++ brick_gym/datasets/list_paths.py
@@ -3,31 +3,24 @@ import os
 import glob
 import json
 
-class PathList:
-    def __init__(self,
-            dataset_directory,
-            split_name,
-            subset=None,
-            rank=0,
-            size=1):
-        dataset_directory = os.path.expanduser(dataset_directory)
-        split_file = os.path.join(dataset_directory, 'splits.json')
-        with open(split_file, 'r') as f:
-            splits = json.load(f)
-        split_glob = splits[split_name]
-        
-        all_file_paths = sorted(glob.glob(os.path.join(
-                dataset_directory, split_glob)))
-        if subset is not None:
-            if isinstance(subset, int):
-                subset = (subset,)
-            all_file_paths = all_file_paths[slice(*subset)]
-        
-        stride = math.ceil(len(all_file_paths) / size)
-        self.file_paths = all_file_paths[rank*stride, (rank+1)*stride]
+def data_paths(self,
+        dataset_directory,
+        split_name,
+        subset=None,
+        rank=0,
+        size=1):
+    dataset_directory = os.path.expanduser(dataset_directory)
+    split_file = os.path.join(dataset_directory, 'splits.json')
+    with open(split_file, 'r') as f:
+        splits = json.load(f)
+    split_glob = splits[split_name]
     
-    def __getitem__(self, index):
-        return self.file_paths[i]
+    all_file_paths = sorted(glob.glob(os.path.join(
+            dataset_directory, split_glob)))
+    if subset is not None:
+        if isinstance(subset, int):
+            subset = (subset,)
+        all_file_paths = all_file_paths[slice(*subset)]
     
-    def __len__(self):
-        return len(self.file_paths)
+    stride = math.ceil(len(all_file_paths) / size)
+    return all_file_paths[rank*stride, (rank+1)*stride]
