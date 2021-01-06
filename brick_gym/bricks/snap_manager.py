9e09754a77ae59415ab02eebbeb10eb4dbcfdaea
Aaron Walsman <aaronwalsman@gmail.com>
2021-01-05 19:09:52 -0800

    commiting to delete this

--- brick_gym/bricks/snap_manager.py
+++ brick_gym/bricks/snap_manager.py
@@ -1,11 +1,9 @@
 from brick_gym.geometry.grid_bucket import GridBucket
-import brick_gym.ldraw.snap as snap
+import brick_gym.bricks.snap as snap
 
 class SnapManager:
     def __init__(self, cell_size=8):
         self.grid_bucket = GridBucket(cell_size)
-    
-    def insert_snap(self, snap_id, snap_position):
-        self.grid_bucket.insert(snap_id, snap_position)
-    
-    def insert_brick(self, 
+        
+    def insert_snap(self, snap_id, snap):
+        
