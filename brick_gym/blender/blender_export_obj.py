ee5e808085e236d2b0a6d4b07ddb58f367f183cd
Aaron Walsman <aaronwalsman@gmail.com>
2021-01-05 19:08:20 -0800

    streamlining things and using config paths

--- brick_gym/blender/blender_export_obj.py
+++ brick_gym/blender/blender_export_obj.py
@@ -5,31 +5,17 @@ import bpy
 
 import io_scene_importldraw.loadldraw.loadldraw as loadldraw
 
+import brick_gym.config as config
+from brick_gym.ldraw.documents import LDrawDocument
+
 # to fix a light/lamp naming issue
 loadldraw.globalLightBricks = {}
 
-part_directory = '/media/awalsman/data_drive/brick_gym/data/ldraw/parts'
-obj_directory = '/media/awalsman/data_drive/brick_gym/data/obj'
-ldraw_directory = '/media/awalsman/data_drive/brick_gym/data/ldraw'
+part_directory = os.path.join(config.paths['ldraw'], 'parts')
+obj_directory = os.path.join(config.paths['renderpy'], 'meshes')
 
 if not os.path.exists(obj_directory):
     os.makedirs(obj_directory)
-
-import_scale = 1.0
-max_export = 10
-
-# import settings
-primitive_resolution = 'Standard'
-smooth_parts = True
-curved_walls = True
-use_logo_studs = False
-bevel_edges = True
-bevel_width = 0.5
-
-# export settings
-use_normals = True
-axis_forward = 'Z'
-axis_up = '-Y'
 
 def clear_scene(scene_name):
     
@@ -54,7 +40,18 @@ def clear_scene(scene_name):
     # OOPS, it also forgets all your plugins!
     #bpy.ops.wm.read_factory_settings(use_empty=True)
 
-def export_brick(brick, overwrite=False):
+def export_brick(brick,
+        overwrite=False,
+        import_scale = 1.0,
+        primitive_resolution = 'Standard',
+        smooth_parts = True,
+        curved_walls = True,
+        use_logo_studs = False,
+        bevel_edges = True,
+        bevel_width = 0.5,
+        use_normals = True,
+        axis_forward = 'Z',
+        axis_up = '-Y'):
     brick_name, brick_ext = os.path.splitext(brick)
     export_brick_path = os.path.join(obj_directory, brick_name + '.obj')
     if overwrite or not os.path.isfile(export_brick_path):
@@ -66,7 +63,7 @@ def export_brick(brick, overwrite=False):
         import_brick_path = os.path.join(part_directory, brick)
         bpy.ops.import_scene.importldraw(
                 filepath = import_brick_path,
-                ldrawPath = ldraw_directory,
+                ldrawPath = config.paths['ldraw'],
                 importScale = import_scale,
                 resPrims = primitive_resolution,
                 smoothParts = smooth_parts,
@@ -86,7 +83,15 @@ def export_brick(brick, overwrite=False):
                 axis_forward = axis_forward,
                 axis_up = axis_up)
 
-def export_carbon_star(overwrite=False):
+def export_scene_bricks(scene_path, **kwargs):
+    scene_document = LDrawDocument.parse_document(scene_path)
+    parts = scene_document.get_all_parts()
+    bricks = [part[0] for part in parts]
+    print(bricks)
+    for brick in bricks:
+        export_brick(brick, **kwargs)
+
+def export_carbon_star(**kwargs):
     for brick in (
             '6157.dat',
             '3031.dat',
@@ -131,22 +136,12 @@ def export_carbon_star(overwrite=False):
             '50951.dat',
             '50951.dat',
             '50951.dat'):
-        export_brick(brick, overwrite=overwrite)
+        export_brick(brick, **kwargs)
 
-def export_test(overwrite=False):
-    for brick in ('87079.dat', '3068b.dat'):
-        export_brick(brick, overwrite=overwrite)
-
-def export_all(overwrite=False):
-    bricks = os.listdir(part_directory)
-    with open('/home/awalsman/tmp.txt', 'w') as f:
-        f.write('\n'.join(bricks))
+def export_all(**kwargs):
+    bricks = sorted(os.listdir(part_directory))
     for brick in bricks:
         import_brick_path = os.path.join(part_directory, brick)
         if os.path.isdir(import_brick_path):
             continue
-        export_brick(brick, overwrite=overwrite)
-    
-        #if brick == '3002.dat':
-        #    print('bailing early!')
-        #    break
+        export_brick(brick, **kwargs)
