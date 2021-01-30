79494ac53c01d4b29892371bc78787c2a9cab165
Aaron Walsman <aaronwalsman@gmail.com>
2021-01-29 17:22:23 -0800

    updates to train script

--- experiments/train_graph_random_stack/train.py
+++ experiments/train_graph_random_stack/train.py
@@ -4,16 +4,35 @@ import brick_gym.torch.train.graph as train_graph
 
 if __name__ == '__main__':
     train_graph.train_label_confidence(
-            train_steps_per_epoch = 1024,
-            #train_subset = 1,
-            test_steps_per_epoch = 16, #512,
-            batch_size = 2,
-            num_processes = 8,
+            # load checkpoints
+            #step_checkpoint = './checkpoint/Jan23_14-30-07_mechagodzilla/'
+            #        'step_model_0010.pt',
+            #edge_checkpoint = './checkpoint/Jan23_14-30-07_mechagodzilla/'
+            #        'edge_model_0010.pt',
+            #optimizer_checkpoint = './checkpoint/Jan23_14-30-07_mechagodzilla/'
+            #        'optimizer_0010.pt',
+            
+            # general settings
             num_epochs = 500,
-            mini_epoch_sequences = 2048,
-            mini_epoch_sequence_length = 2,
             mini_epochs_per_epoch = 1,
+            mini_epoch_sequences = 512,
+            mini_epoch_sequence_length = 4,
+            
+            # dataset settings
             dataset = 'random_stack',
+            num_processes = 8,
             randomize_viewpoint=False,
+            
+            # train settings
+            train_steps_per_epoch = 1024,
+            batch_size = 2,
+            
+            # test settings
+            test_frequency = None,
+            test_steps_per_epoch = 16, #512,
+            
+            # logging settings
             log_train=8,
+            
+            # checkpoint settings
             checkpoint_frequency=5)
