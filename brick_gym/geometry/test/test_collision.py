737a05f0c36bc87ae5532f7198c187c0857578fb
Aaron Walsman <aaronwalsman@gmail.com>
2021-03-01 13:09:58 -0800

    now dumps images

--- brick_gym/geometry/test/test_collision.py
+++ brick_gym/geometry/test/test_collision.py
@@ -14,9 +14,17 @@ scene = BrickScene(renderable=True)
 scene.import_ldraw(os.path.join(
         config.paths['omr'], 'ldraw', '8661-1 - Carbon Star.mpd'))
 
-instance = scene.instances['23']
+instance = scene.instances['29']
 #instance.transform[2,3] -= 10
 scene.renderer.set_instance_transform(str(instance), instance.transform)
 snap = instance.get_snaps()[1]
 
-print(check_collision(scene, instance, snap.transform, 'F'))
+collision = check_collision(
+        scene,
+        [instance],
+        snap.transform,
+        'F',
+        resolution=(512,512),
+        dump_images='test')
+
+print(collision)
