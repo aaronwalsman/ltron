ef2ef679ac875e4c9687f7a2cdea01165214905a
Aaron Walsman <aaronwalsman@gmail.com>
2021-03-01 13:20:32 -0800

    rando_micro_wheels experiments

---
+++ experiments/rando_micro_wheels/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#import brick_gym.torch.train.graph_d as graph_d
+import brick_gym.torch.train.test_graph as test_graph
+
+#run = 'Jan24_01-28-20_mechagodzilla'
+#epoch = 200
+#run = 'Feb10_11-50-54_gpu3'
+#run = 'Feb23_00-57-59_mechagodzilla' #'Feb17_22-57-19_gpu3'
+run = 'Feb28_23-41-32_mechagodzilla'
+epoch = 475
+
+if __name__ == '__main__':
+    test_graph.test_checkpoint(
+            # load checkpoints
+            step_checkpoint = './checkpoint/%s/step_model_%04i.pt'%(run, epoch),
+            edge_checkpoint = './checkpoint/%s/edge_model_%04i.pt'%(run, epoch),
+            
+            # dataset settings
+            dataset = 'rando_micro_wheels',
+            num_processes = 4,
+            test_split = 'all',
+            test_subset = 32,
+            
+            # model settings
+            step_model_name='nth_try',
+            decoder_channels=512,
+            step_model_backbone='simple',
+            segment_id_matching=False,
+            
+            # output settings
+            dump_debug=True)
---
+++ experiments/rando_micro_wheels/train.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#import brick_gym.torch.train.graph_d as graph_d
+import brick_gym.torch.train.graph as train_graph
+
+run = None
+epoch = 0
+
+if run is not None:
+    step_checkpoint = './checkpoint/%s/step_model_%04i.pt'%(run, epoch)
+    edge_checkpoint = './checkpoint/%s/edge_model_%04i.pt'%(run, epoch)
+    optimizer_checkpoint = './checkpoint/%s/optimizer_%04i.pt'%(run, epoch)
+else:
+    step_checkpoint = None
+    edge_checkpoint = None
+    optimizer_checkpoint = None
+
+if __name__ == '__main__':
+    train_graph.train_label_confidence(
+            # load checkpoints
+            step_checkpoint = step_checkpoint,
+            edge_checkpoint = edge_checkpoint,
+            optimizer_checkpoint = optimizer_checkpoint,
+            
+            # general settings
+            num_epochs = 500,
+            mini_epochs_per_epoch = 1,
+            
+            # dataset settings
+            dataset = 'rando_micro_wheels',
+            train_split = 'all',
+            train_subset = None,
+            num_processes = 4,
+            randomize_viewpoint=True,
+            
+            #load_scenes=False,
+            #random_floating_bricks=False,
+            #random_floating_pairs=False,
+            #random_bricks_rotation_mode='identity',
+            #random_bricks_per_scene=(20,30),
+            
+            # rollout settings
+            train_steps_per_epoch = 1024,
+            
+            # train settings
+            learning_rate = 1e-4,
+            weight_decay = 1e-6,
+            mini_epoch_sequences = 2048,
+            mini_epoch_sequence_length = 1,
+            batch_size = 32,
+            #edge_loss_weight = 0.,
+            #matching_loss_weight = 0.,
+            multi_hide = True,
+            max_instances_per_step=4,
+            
+            # model settings
+            #step_model_backbone = 'smp_fpn_rnxt50',
+            step_model_backbone = 'simple',
+            edge_model_name = 'squared_difference',
+            segment_id_matching = False,
+            
+            # test settings
+            test_frequency = None,
+            test_steps_per_epoch = 8, #512,
+            
+            # logging settings
+            log_train=8,
+            
+            # checkpoint settings
+            checkpoint_frequency=5)
