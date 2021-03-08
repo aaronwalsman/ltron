8d9d4ac4b3cc141bebd43195d5b95469cdc9d98c
Aaron Walsman <aaronwalsman@gmail.com>
2021-03-08 13:49:13 -0800

    adding viewpoint control

--- brick_gym/gym/components/viewpoint.py
+++ brick_gym/gym/components/viewpoint.py
@@ -7,6 +7,103 @@ import renderpy.camera as camera
 
 from brick_gym.gym.components.brick_env_component import BrickEnvComponent
 
+class ControlledAzimuthalViewpointComponent(BrickEnvComponent):
+    def __init__(self,
+            scene_component,
+            azimuth_steps,
+            elevation_range,
+            elevation_steps,
+            distance_range,
+            distance_steps,
+            field_of_view = math.radians(60.),
+            aspect_ratio = 1.,
+            near_clip = 1.,
+            far_clip = 5000.,
+            start_position = 'uniform'):
+        
+        self.scene_component = scene_component
+        self.azimuth_range = azimuth_range
+        self.azimuth_steps = azimuth_steps
+        self.elevation_range = elevation_range
+        self.elevation_steps = elevation_steps
+        self.distance_range = distance_range
+        self.distance_steps = distance_steps
+        self.field_of_view = field_of_view
+        self.aspect_ratio = aspect_ratio
+        self.near_clip = near_clip
+        self.far_clip = far_clip
+        self.start_position = start_position
+        
+        self.action_space = spaces.Discrete(7)
+        self.num_locations = azimuth_steps * elevation_steps * distance_steps
+        self.location = None
+        
+        self.azimuth_spacing = math.pi * 2 / azimuth_steps
+        self.elevation_spacing = (
+                elevation_range[1] - elevation_range[0]) / (elevation_steps-1)
+        self.distance_spacing = (
+                distance_range[1] - distance_range[2]) / (distance_steps-1)
+    
+    def reset(self):
+        if self.start_position == 'uniform':
+            self.position = [
+                    random.randint(0, self.azimuth_steps-1),
+                    random.randint(0, self.elevation_steps-1),
+                    random.randint(0, self.distance_steps-1)]
+        else:
+            self.position = list(start_position)
+        self.set_camera()
+        
+        return None
+    
+    def step(self, action):
+        if action == 0:
+            pass
+        elif action == 1:
+            self.position[0] -= 1
+            self.position[0] = self.position[0] % self.azimuth_steps
+        elif action == 2:
+            self.position[0] += 1
+            self.position[0] = self.position[0] % self.azimuth_steps
+        elif action == 3:
+            self.position[1] -= 1
+            self.position[1] = max(0, self.position[1])
+        elif action == 4:
+            self.position[1] += 1
+            self.position[1] = min(self.elevation_steps-1, self.position[1])
+        elif action == 5:
+            self.position[2] -= 1
+            self.position[2] = max(0, self.position[2])
+        elif action == 6:
+            self.position[2] += 1
+            self.position[2] = min(self.distance_steps-1, self.position[2])
+        
+        self.set_camera()
+        
+        return None, 0, False, None
+    
+    def set_camera(self):
+        scene = self.scene_component.brick_scene
+        azimuth = random.uniform(*self.azimuth)
+        elevation = random.uniform(*self.elevation)
+        tilt = random.uniform(*self.tilt)
+        field_of_view = random.uniform(*self.field_of_view)
+        distance_scale = random.uniform(*self.distance)
+        
+        # projection
+        self.projection = camera.projection_matrix(
+                self.field_of_view,
+                self.aspect_ratio,
+                self.near_clip,
+                self.far_clip)
+        scene.set_projection(self.projection)
+        
+        # pose
+        camera_pose = camera.azimuthal_pose_to_matrix(
+                [azimuth, elevation, tilt, distance, 0.0, 0.0],
+                center = center)
+        scene.set_camera_pose(camera_pose)
+
 class RandomizedAzimuthalViewpointComponent(BrickEnvComponent):
     def __init__(self,
             scene_component,
