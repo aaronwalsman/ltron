a8f7c4d071db6e0d958d9203be77ea22e996485f
Aaron Walsman <aaronwalsman@gmail.com>
2021-04-28 10:57:42 -0700

    removing comments

--- ltron/geometry/utils.py
+++ ltron/geometry/utils.py
@@ -18,19 +18,3 @@ def uniform_unit_quaternion():
         q = [random.rand() for _ in range(4)]
         l22 = squared_distance(q, [0,0,0,0])
     return q / (l22**0.5)
-
-'''
-# USE PYQUATERNION
-def quaternion_to_matrix(q):
-    # Martin Baker!
-    qx, qy, qz, qw = q
-    qx2, qy2, qz2, qw2 = qx**2, qy**2, qz**2, qw**2
-    return numpy.array([
-        [1 - 2*qy2 - 2*qz2, 2*qx*qy - 2*qz*qw, 2*qx*qz + 2*qy*qw, 0],
-        [2*qx*qy + 2*qz*qw, 1 - 2*qx2 - 2*qz2, 2*qy*qz - 2*qx*qw, 0],
-        [2*qx*qz - 2*qy*qw, 2*qy*qz + 2*qx*qw, 1 - 2*qx2 - 2*qy2, 0],
-        [                0,                 0,                 0, 1]])
-
-def uniform_rotation_matrix():
-    return quaternion_to_matrix(uniform_unit_quaternion())
-'''
