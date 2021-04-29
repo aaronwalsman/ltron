b6dfc7f7f475b6f0b8b81a42d7d81cdc393af41d
Aaron Walsman <aaronwalsman@gmail.com>
2021-04-28 17:01:24 -0700

    cleanup

--- ltron/ldraw/commands.py
+++ ltron/ldraw/commands.py
@@ -1,7 +1,7 @@
 import re
 
 import numpy
-from ltron.ldraw.exceptions import *
+from ltron.ldraw.exceptions import LDrawException
 import ltron.ldraw.paths as ldraw_paths
 
 class InvalidLDrawCommand(LDrawException):
@@ -195,31 +195,15 @@ class LDCadSnapStyleCommand(LDCadCommand):
         super(LDCadSnapStyleCommand, self).__init__(ldcad_command, flags)
         self.id = flags.get('id', '')
         self.transform = matrix_ldcad_to_numpy(flags)
-        '''
-        if gender is not None:
-            self.gender = gender
-        else:
-            self.gender = flags['gender']
-        '''
 
 class LDCadSnapCylCommand(LDCadSnapStyleCommand):
     pass
 
 class LDCadSnapClpCommand(LDCadSnapStyleCommand):
     pass
-    '''
-    def __init__(self, ldcad_command, flags):
-        super(LDCadSnapClpCommand, self).__init__(
-                ldcad_command, flags, 'F')
-    '''
 
 class LDCadSnapFgrCommand(LDCadSnapStyleCommand):
     pass
-    '''
-    def __init__(self, ldcad_command, flags):
-        super(LDCadSnapFgrCommand, self).__init__(
-                ldcad_command, flags, flags['genderofs'])
-    '''
 
 class LDCadSnapGenCommand(LDCadSnapStyleCommand):
     pass
