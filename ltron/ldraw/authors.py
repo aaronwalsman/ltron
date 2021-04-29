ef5ecb1aa5cab5edcc5e784b1e18688defa9d22c
Aaron Walsman <aaronwalsman@gmail.com>
2021-04-28 20:39:53 -0700

    making this a library instead of a stand-alone script

--- ltron/ldraw/authors.py
+++ ltron/ldraw/authors.py
@@ -1,28 +1,36 @@
-#!/usr/bin/env python
 import tqdm
 
 from ltron.ldraw.paths import LDRAW_FILES, SHADOW_FILES
 from ltron.ldraw.documents import LDrawDocument
 from ltron.ldraw.commands import LDrawAuthorComment
 
-all_authors = {}
+def get_all_ldraw_authorship():
+    library_authors = {}
+    for library_name, part_files in (
+        ('shadow', SHADOW_FILES.values()),
+        ('ldraw', LDRAW_FILES.values()),
+    ):
+        library_authors[library_name] = get_ldraw_authorship(part_files)
+    
+    return library_authors
 
-for part in tqdm.tqdm(SHADOW_FILES.values()):
-    try:
-        doc = LDrawDocument.parse_document(part)
-    except ValueError:
-        print('skipping: %s'%part)
-        continue
-    for command in doc.commands:
-        if isinstance(command, LDrawAuthorComment):
-            if command.author not in all_authors:
-                all_authors[command.author] = []
-            all_authors[command.author].append(part)
-            break
+def get_ldraw_authorship(part_files):
+    all_authors = {'UNKNOWN':[]}
     
-    else:
-        print('No author found for: %s'%part)
+    for part in tqdm.tqdm(part_files):
+        try:
+            doc = LDrawDocument.parse_document(part)
+        except ValueError:
+            print('skipping: %s'%part)
+            continue
+        for command in doc.commands:
+            if isinstance(command, LDrawAuthorComment):
+                if command.author not in all_authors:
+                    all_authors[command.author] = []
+                all_authors[command.author].append(part)
+                break
+        
+        else:
+            all_authors['UNKNOWN'].append(part)
     
-for author, parts in sorted(
-        all_authors.items(), key=lambda x : len(x[1]), reverse=True):
-    print(author.replace('_', '\\_') + ',')
+    return all_authors
