86e41f3a7236ddb49ee4b901fa4b00f2f29bbccc
Aaron Walsman <aaronwalsman@gmail.com>
2021-06-19 18:27:39 -0700

    updating installation code

--- ltron/installation.py
+++ ltron/installation.py
@@ -14,85 +14,6 @@ from ltron.home import get_ltron_home, make_ltron_home
 
 ltron_home = get_ltron_home()
 
-'''
-def make_data_paths():
-    print('='*80)
-    print('Making paths')
-    print('-'*80)
-    if not os.path.isdir(settings.paths['data']):
-        print('Making data directory: %s'%settings.paths['data'])
-        os.makedirs(settings.paths['data'])
-    else:
-        print('Data directory already exists: %s'%settings.paths['data'])
-
-    print('-'*80)
-    if not os.path.isdir(settings.paths['collections']):
-        print('Making collections directory: %s'%settings.paths['collections'])
-        os.makedirs(settings.paths['collections'])
-    else:
-        print(
-            'Collections directory already exists: %s'%
-            settings.paths['collections']
-        )
-
-def download(name, destination, url, overwrite=False, mode='request'):
-    if os.path.exists(destination):
-        if overwrite:
-            print('Removing existing %s: %s'%(name, destination))
-            os.remove(destination)
-        else:
-            print('%s already downloaded: %s'%(name, destination))
-    if not os.path.exists(destination):
-        print('Downloading %s to: %s'%(name, destination))
-        if mode == 'request':
-            r = requests.get(str(url), allow_redirects=True)
-            open(destination, 'wb').write(r.content)
-        elif mode == 'gdown':
-            gdown.cached_download(url, destination, quiet=False)
-'''
-
-'''
-def extract(
-    name,
-    source_path,
-    extract_name,
-    destination,
-    overwrite=False,
-    mode='zip'
-):
-    if os.path.exists(destination):
-        if overwrite:
-            print('Removing existing %s: %s'%(name, destination))
-            shutil.rmtree(destination)
-        else:
-            print('%s already exists: %s'%(name, destination))
-    if not os.path.exists(destination):
-        source_dir = os.path.dirname(source_path)
-        extract_path = os.path.join(source_dir, extract_name)
-        print('Extracting %s contents to: %s'%(name, extract_path))
-        if mode == 'zip':
-            with zipfile.ZipFile(source_path, 'r') as z:
-                z.extractall(source_dir)
-        elif mode == 'bz2':
-            with open(source_path, 'rb') as f_in:
-                data = bz2.decompress(f_in.read())
-                with open(extract_path, 'wb') as f_out:
-                    f_out.write(data)
-        elif mode == 'tar':
-            with tarfile.open(source_path, 'r:') as t:
-                t.extractall(source_dir)
-        else:
-            raise NotImplementedError
-        if destination != extract_path:
-            destination_directory = os.path.dirname(destination)
-            if not os.path.exists(destination_directory):
-                print('Making directory nescessary for %s location: %s'%(
-                    name, destination_directory))
-                os.makedirs(destination_directory)
-            print('Moving %s to: %s'%(name, destination))
-            os.rename(extract_path, destination)
-'''
-
 def install_ldraw(overwrite=False):
     print('='*80)
     print('Installing LDraw')
@@ -115,32 +36,6 @@ def install_ldraw(overwrite=False):
                 z.extractall(ltron_home)
         else:
             print('Must agree to all licensing.  Aborting LDraw install.')
-
-'''
-def install_shadow(overwrite=False):
-    raise Exception('Needs overhaul, pull directly from Melkert')
-    print('='*80)
-    print('Installing LDCad Shadow Files')
-    
-    print('-'*80)
-    shadow_zip_path = os.path.join(settings.paths['data'], 'shadow.zip')
-    download(
-        'Shadow',
-        shadow_zip_path,
-        settings.urls['shadow'],
-        overwrite=overwrite,
-    )
-    
-    print('-'*80)
-    extract(
-        'Shadow',
-        shadow_zip_path,
-        'shadow',
-        settings.paths['shadow'],
-        overwrite=overwrite,
-        mode='zip',
-    )
-'''
 
 def ldcad_license_agreement():
     ldcad_license_text = '''LDCad license agreement (V3)
@@ -202,6 +97,7 @@ def install_ldcad(overwrite=True):
     with tarfile.open(ldcad_tar_path, 'r:') as f:
         f.extractall(ltron_home)
     
+    # unzip shadow
     print('-'*80)
     print('Unzipping shadow')
     shadow_seed_path = os.path.join(ldcad_path, 'seeds', 'shadow.sf')
@@ -212,30 +108,17 @@ def install_ldcad(overwrite=True):
         with zipfile.ZipFile(shadow_seed_path, 'r') as z:
             z.extractall(ldcad_shadow_path)
     
-    '''
+    # unzip offLib
     print('-'*80)
-    ldcad_tar = os.path.join(settings.paths['data'], '%s.tar'%ldcad_name)
-    extract(
-        'LDCad tar',
-        ldcad_bz2,
-        '%s.tar'%ldcad_name,
-        ldcad_tar,
-        overwrite=overwrite,
-        mode='bz2',
-    )
-    '''
-    '''
-    print('-'*80)
-    ldcad_path = os.path.join(settings.paths['data'], ldcad_name)
-    extract(
-        'LDCad',
-        ldcad_tar,
-        ldcad_name,
-        settings.paths['ldcad'],
-        overwrite=overwrite,
-        mode='tar',
-    )
-    '''
+    print('Unzipping offLibShadow')
+    ldcad_offlibshadow_csl_path = os.path.join(
+        ldcad_shadow_path, 'offLib', 'offLibShadow.csl')
+    ldcad_offlibshadow_path = os.path.join(
+        ldcad_shadow_path, 'offLib', 'offLibShadow')
+    if not os.path.exists(ldcad_offlibshadow_path):
+        os.makedirs(ldcad_offlibshadow_path)
+        with zipfile.ZipFile(ldcad_offlibshadow_csl_path, 'r') as z:
+            z.extractall(ldcad_offlibshadow_path)
 
 def install_collection(name, overwrite=False):
     print('='*80)
