75513e9ea20c4c098ef0d918fa5b12d9c4109b4d
nanami <nanami17@cs.washington.edu>
2021-08-07 16:10:57 -0700

    Separate components into its own file

--- ltron/gym/components/build_tower.py
+++ ltron/gym/components/build_tower.py
@@ -1,15 +1,5 @@
 import numpy
 from ltron.gym.components.ltron_gym_component import LtronGymComponent
-from gym.spaces import (
-    Discrete,
-    Tuple,
-    Dict,
-    MultiDiscrete
-)
-from ltron.gym.spaces import (
-    SinglePixelSelectionSpace,
-)
-import math
 
 class TallestTower(LtronGymComponent):
     def __init__(self, scenecomponent):
@@ -56,52 +46,3 @@ class TallestTower(LtronGymComponent):
     def step(self, action):
         return None, self.compute_reward(), False, None
 
-class PickandPlace(LtronGymComponent):
-    def __init__(self, scene, pos_snap_render, neg_snap_render):
-        self.scene_component = scene
-        self.action_executed = 0
-        self.pos_snap_render = pos_snap_render
-        self.neg_snap_render = neg_snap_render
-        self.instance_pos = {}
-        width = self.pos_snap_render.width
-        height = self.pos_snap_render.height
-        assert self.neg_snap_render.width == width
-        assert self.neg_snap_render.height == height
-
-        pick_polarity_space = Discrete(2)
-        pick_space = SinglePixelSelectionSpace(width, height)
-        place_space = SinglePixelSelectionSpace(width, height)
-        # self.action_space = Tuple(
-        #     (pick_polarity_space, pick_space, place_space))
-        self.action_space = MultiDiscrete([2, width, height, width, height])
-        # self.action_space = MultiDiscrete([2, 20, 20, 20, 20])
-
-        self.observation_space = Dict({'pick_place_succeed': Discrete(2)})
-
-    def reset(self):
-        return None, 0, False, None
-
-    def step(self, action):
-        if action is None: return None, 0, False, None
-        polarity = action[0]  # Integer, 0 or 1
-        # pick_x, pick_y = action[1]+100, action[2]+100 # a tuple, corresponding to the coordinate of pick location
-        # place_x, place_y = action[3]+150, action[4]+150 # a tuple, corresponding to the coordinate of place location
-        pick_x, pick_y = action[1], action[2]
-        place_x, place_y = action[3], action[4]
-
-        if polarity == 1:
-            pick_map = self.pos_snap_render.observation
-            place_map = self.neg_snap_render.observation
-        else:
-            pick_map = self.neg_snap_render.observation
-            place_map = self.pos_snap_render.observation
-
-        pick_instance, pick_id = pick_map[pick_x, pick_y]
-        place_instance, place_id = place_map[place_x, place_y]
-
-        if pick_instance == 0 or pick_id == 0 or place_instance == 0 or place_id == 0:
-            return {'pick_place_succeed' : 0}, 0, False, None
-
-        self.scene_component.brick_scene.pick_and_place_snap((pick_instance, pick_id), (place_instance, place_id))
-
-        return {'pick_place_suceed' : 1}, 0, False, None  # the observation is whether the action succeeds or not
