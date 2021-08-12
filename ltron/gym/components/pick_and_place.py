fc106b0a1eb04ca1fb6a7de283bda89e17823e09
Aaron Walsman <aaronwalsman@gmail.com>
2021-08-11 23:32:12 -0700

    Aaron edits

--- ltron/gym/components/pick_and_place.py
+++ ltron/gym/components/pick_and_place.py
@@ -23,43 +23,47 @@ class PickandPlace(LtronGymComponent):
         self.height = self.pos_snap_render.height
         assert self.neg_snap_render.width == self.width
         assert self.neg_snap_render.height == self.height
-
+        
+        activate_space = Discrete(2)
         pick_polarity_space = Discrete(2)
         pick_space = SinglePixelSelectionSpace(self.width, self.height)
         place_space = SinglePixelSelectionSpace(self.width, self.height)
-        # self.action_space = Tuple(
-        #     (pick_polarity_space, pick_space, place_space))
-        self.action_space = MultiDiscrete([2, self.width, self.height, self.width, self.height])
+        self.action_space = Tuple(
+            (activate_space, pick_polarity_space, pick_space, place_space))
+        #self.action_space = MultiDiscrete([2, self.width, self.height, self.width, self.height])
         # self.action_space = MultiDiscrete([2, 20, 20, 20, 20])
 
-        self.observation_space = Dict({'pick_place_succeed': Discrete(2)})
+        self.observation_space = Dict({'success': Discrete(2)})
 
     def reset(self):
-        return None, 0, False, None
+        return {'success':False}
 
     def step(self, action):
-        if action is None: return None, 0, False, None
-        polarity = action[0]  # Integer, 0 or 1
+        #if action is None: return None, 0, False, None
+        #polarity = action[0]  # Integer, 0 or 1
         # pick_x, pick_y = action[1]+100, action[2]+100 # a tuple, corresponding to the coordinate of pick location
         # place_x, place_y = action[3]+150, action[4]+150 # a tuple, corresponding to the coordinate of place location
-        pick_x, pick_y = action[1], action[2]
-        place_x, place_y = action[3], action[4]
-
+        #pick_x, pick_y = action[1], action[2]
+        #place_x, place_y = action[3], action[4]
+        
+        activate, polarity, (pick_y, pick_x), (place_y, place_x) = action
+        
         if polarity == 1:
             pick_map = self.pos_snap_render.observation
             place_map = self.neg_snap_render.observation
         else:
             pick_map = self.neg_snap_render.observation
             place_map = self.pos_snap_render.observation
-
-        pick_instance, pick_id = pick_map[pick_x, pick_y]
-        place_instance, place_id = place_map[place_x, place_y]
+        
+        pick_instance, pick_id = pick_map[pick_y, pick_x]
+        place_instance, place_id = place_map[place_y, place_x]
         # if check_collision(self.scene_component.brick_scene, pick_instance, abs(polarity - 1), (self.width, self.height)):
         #     return {'pick_place_succeed': 0}, 0, False, None
-
-        if pick_instance == 0 or pick_id == 0 or place_instance == 0 or place_id == 0:
-            return {'pick_place_succeed' : 0}, 0, False, None
-
-        self.scene_component.brick_scene.pick_and_place_snap((pick_instance, pick_id), (place_instance, place_id))
-
-        return {'pick_place_suceed' : 1}, 0, False, None  # the observation is whether the action succeeds or not
+        
+        if pick_instance == 0 or place_instance == 0:
+            return {'success' : 0}, 0, False, None
+        
+        self.scene_component.brick_scene.pick_and_place_snap(
+            (pick_instance, pick_id), (place_instance, place_id))
+        
+        return {'success' : 1}, 0, False, None  # the observation is whether the action succeeds or not
