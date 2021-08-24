9eba752a58fc00f3bee7d11d7dadedc71bdf7ada
nanami <nanami17@cs.washington.edu>
2021-08-23 17:48:44 -0700

    Non-overlap extraction

--- ltron/dataset/submodel_extraction.py
+++ ltron/dataset/submodel_extraction.py
@@ -16,6 +16,54 @@ import copy
 import collections
 import math
 import os
+import numpy
+
+
+def compute_boxsize(instances, scene):
+    instance_tran = {}
+    for k in instances:
+        instance_tran[k] = scene.instances.instances[k].transform
+
+    instance_pos = {}
+    for k in instances:
+        instance_pos[k] = scene.instances.instances[k].brick_type.bbox
+
+    point = []
+    for ins, bbox in instance_pos.items():
+        minb = bbox[0]
+        maxb = bbox[1]
+        point.append((numpy.matmul(instance_tran[ins], numpy.array([minb[0], minb[1], minb[2], 1])))[:3])
+        point.append((numpy.matmul(instance_tran[ins], numpy.array([maxb[0], minb[1], minb[2], 1])))[:3])
+        point.append((numpy.matmul(instance_tran[ins], numpy.array([minb[0], maxb[1], minb[2], 1])))[:3])
+        point.append((numpy.matmul(instance_tran[ins], numpy.array([minb[0], minb[1], maxb[2], 1])))[:3])
+        point.append((numpy.matmul(instance_tran[ins], numpy.array([maxb[0], maxb[1], minb[2], 1])))[:3])
+        point.append((numpy.matmul(instance_tran[ins], numpy.array([minb[0], maxb[1], maxb[2], 1])))[:3])
+        point.append((numpy.matmul(instance_tran[ins], numpy.array([maxb[0], minb[1], maxb[2], 1])))[:3])
+        point.append((numpy.matmul(instance_tran[ins], numpy.array([maxb[0], maxb[1], maxb[2], 1])))[:3])
+
+    min_y = 100000
+    max_y = -1000000
+    min_x = 100000
+    max_x = -100000
+    min_z = 100000
+    max_z = -1000000
+    for p in point:
+        if p[1] > max_y:
+            max_y = p[1]
+        if p[1] < min_y:
+            min_y = p[1]
+        if p[0] > max_x:
+            max_x = p[0]
+        if p[0] < min_x:
+            min_x = p[0]
+        if p[2] > max_z:
+            max_z = p[2]
+        if p[2] < max_z:
+            min_z = p[2]
+
+    # if abs(max_y - min_y) - 35 > 0: return 10000
+    # else: return -1000
+    return max(abs(max_y - min_y), abs(max_x - min_x), abs(max_z - min_z))
 
 def add_brick(limit, cur_mod, comp_list, instance_id, scene):
     instance = scene.instances.instances[instance_id]
@@ -32,6 +80,31 @@ def add_brick(limit, cur_mod, comp_list, instance_id, scene):
         else:
             add_brick(limit, cur_mod, comp_list, target, scene)
             return True
+
+def add_brick_box(limit, cur_mod, comp_list, instance_id, scene):
+    instance = scene.instances.instances[instance_id]
+    connections = scene.get_all_snap_connections([instance])[str(instance_id)]
+    if len(connections) == 0: return False
+    box_map = {}
+    for conn in connections:
+        temp = copy.deepcopy(cur_mod)
+        target = int(conn[0])
+        if target in cur_mod:
+            continue
+        temp.append(target)
+        box_map[target] = compute_boxsize(temp, scene)
+
+    if len(box_map) == 0: return False
+    best_target = min(box_map, key=box_map.get)
+    cur_mod.append(best_target)
+    if len(cur_mod) >= limit:
+        comp_list.append(cur_mod)
+        return True
+    else:
+        add_brick_box(limit, cur_mod, comp_list, best_target, scene)
+        return True
+
+    return False
 
 # limit - number of bricks in each subcomponent
 # num_comp - number of subcomponents want to generate(won't run over this number)
@@ -91,6 +164,70 @@ def subcomponent_extraction(limit, num_comp):
         if global_count == num_comp:
             break
 
+def subcomponent_nonoverlap_extraction(limit, num_comp):
+    global_count = 0
+    folder_name = "subcomponents" + str(limit) + "/"
+    try:
+        os.stat(folder_name)
+    except:
+        os.mkdir(folder_name)
+    path = Path("~/.cache/ltron/collections/omr/ldraw").expanduser()
+    mpdlist = path.rglob('*mpd') # 1432
+    stat = {"error" : []}
+
+    # Iterate through mpd files
+    for mpd in mpdlist:
+        print(mpd)
+        mpd = str(mpd)
+        try:
+            mpdfile = LDrawMPDMainFile(mpd)
+            scene = BrickScene(track_snaps=True)
+            scene.import_ldraw(mpd)
+        except:
+            stat['error'].append(mpd)
+            continue
+
+        num_instances = len(scene.instances.instances)
+        components = []
+        used_brick = []
+        for i in range(num_instances):
+            cur_list = []
+            add_brick(limit, [], cur_list, i+1, scene)
+            for subcomp in cur_list:
+                if global_count == num_comp:
+                    break
+                subcomp.sort()
+                if subcomp not in components:
+                    decider = True
+                    for ins in subcomp:
+                        if ins in used_brick:
+                            decider = False
+                            break
+                        used_brick.append(ins)
+                    if decider:
+                        components.append(subcomp)
+                        global_count += 1
+
+            if global_count == num_comp:
+                break
+
+        count = 1
+        modelname = mpd.split("/")[-1][:-4]
+        for comp in components:
+            temp_scene = BrickScene()
+            temp_scene.import_ldraw(mpd)
+            instances = copy.deepcopy(temp_scene.instances.instances)
+            for k, v in instances.items():
+                if k not in comp:
+                    temp_scene.remove_instance(v)
+
+            temp_scene.export_ldraw(folder_name + modelname + "_"
+                                                            + str(count) + ".mpd")
+            count += 1
+
+        if global_count == num_comp:
+            break
+
 # Render a .mpd file
 def render(filepath):
     components = collections.OrderedDict()
@@ -110,9 +247,12 @@ def render(filepath):
     obs = env.reset()
     imshow(obs['render'])
     plt.show()
+    # print(compute_boxsize(components['scene'].brick_scene.instances.instances.keys(), components['scene'].brick_scene))
 
 def main():
-    subcomponent_extraction(8, 50)
+    subcomponent_nonoverlap_extraction(8, 4)
+    render("subcomponents8/6954-1 - Renegade_1.mpd")
+    render("subcomponents8/6954-1 - Renegade_2.mpd")
 
 if __name__ == '__main__' :
     main()
