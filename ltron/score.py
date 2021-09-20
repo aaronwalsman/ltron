1b3b4f4c99a4b7bd60bdeffa2a1e46156ee272ed
Aaron Walsman <aaronwalsman@gmail.com>
2021-09-20 10:12:47 -0700

    some updates to score, but it's out of date, need to incorporate new matching function

--- ltron/score.py
+++ ltron/score.py
@@ -65,7 +65,7 @@ def score_brick_assignment(
     y_brick_type,
     y_brick_color,
     y_neighbors,
-    wrong_pose_discount=0.0
+    wrong_pose_discount=0.
 ):
     
     if str(x_brick_type) != str(y_brick_type):
@@ -74,6 +74,12 @@ def score_brick_assignment(
     if str(x_brick_color) != str(y_brick_color):
         return 0.
     
+    if not len(x_neighbors):
+        if not len(y_neighbors):
+            return 1.
+        else:
+            return 0.
+    
     xy_scores = numpy.zeros((len(x_neighbors), len(y_neighbors)))
     for x, x_neighbor in enumerate(x_neighbors):
         for y, y_neighbor in enumerate(y_neighbors):
@@ -81,12 +87,24 @@ def score_brick_assignment(
                 x_neighbor, y_neighbor, wrong_pose_discount)
     
     x_scores, y_scores, x_best, y_best = compute_matching(xy_scores)
-    return pseudo_f1(x_scores, y_scores)
+    pf1 = pseudo_f1(x_scores, y_scores)
+    return pf1
+
+def get_max_configuration_instance(configuration):
+    try:
+        return numpy.max(numpy.where(configuration['class'] != 0))
+    except ValueError:
+        return 0
 
 def get_neighbors(configuration):
     neighbors = []
-    for i in range(1, configuration['num_instances']+1):
-        edges = configuration['edges']['edge_index']
+    #edges = configuration['edges']['edge_index']
+    edges = configuration['edges']
+    #for i in range(1, configuration['num_instances']+1):
+    #print(numpy.unique(edges[0]))
+    #for i in numpy.unique(edges[0]):
+    max_x = get_max_configuration_instance(configuration)
+    for i in range(1, max_x+1):
         edge_locations = numpy.where(edges[0] == i)[0]
         neighbor_ids = edges[1,edge_locations]
         if len(neighbor_ids):
@@ -111,6 +129,7 @@ def score_configurations(
     y_configuration,
     wrong_pose_discount=0.0
 ):
+    
     # for each brick in the list of x_bricks, get the offset, color and type
     # of all neighbors
     #x_offsets = [
@@ -127,10 +146,18 @@ def score_configurations(
     y_neighbors = get_neighbors(y_configuration)
     # score every pairwise association between bricks
     #xy_scores = numpy.zeros((len(x_bricks), len(y_bricks)))
-    xy_scores = numpy.zeros(
-        (x_configuration['num_instances'], y_configuration['num_instances']))
-    for x in range(1, x_configuration['num_instances']+1):
-        for y in range(1, y_configuration['num_instances']+1):
+    
+    #num_x = numpy.max(numpy.where(x_configuration['class'] != 0))
+    #num_y = numpy.max(numpy.where(y_configuration['class'] != 0))
+    max_x = get_max_configuration_instance(x_configuration)
+    max_y = get_max_configuration_instance(y_configuration)
+    xy_scores = numpy.zeros((max_x, max_y))
+    #xy_scores = numpy.zeros(
+    #    (x_configuration['num_instances'], y_configuration['num_instances']))
+    #for x in range(1, x_configuration['num_instances']+1):
+    #    for y in range(1, y_configuration['num_instances']+1):
+    for x in range(1, max_x+1):
+        for y in range(1, max_y+1):
             xy_scores[x-1, y-1] = score_brick_assignment(
                 x_configuration['class'][x],
                 x_configuration['color'][x],
@@ -139,6 +166,7 @@ def score_configurations(
                 y_configuration['color'][y],
                 y_neighbors[y-1],
             )
+    
     '''
     for x, (x_brick, x_offset) in enumerate(zip(x_bricks, x_offsets)):
         for y, (y_brick, y_offset) in enumerate(zip(y_bricks, y_offsets)):
