c23fd73e0026e6a4ad8f719b6c02af93cdbb61b0
Aaron Walsman <aaronwalsman@gmail.com>
2021-12-06 20:30:41 -0800

    renaming

--- ltron/matching.py
+++ ltron/matching.py
@@ -8,28 +8,33 @@ from scipy.spatial import cKDTree
 
 from ltron.geometry.utils import default_allclose
 
-def match_configurations(
-    config_a,
-    config_b,
+def match_assemblies(
+    assembly_a,
+    assembly_b,
     kdtree=None,
     radius=0.01,
 ):
     '''
-    Note: This is optimized for the case where config_b is larger than config_a
+    Computes the rigid offset between two assemblies that brings as many of
+    their bricks into alignment as possible.  The naive implementation would
+    use N^2 checks to test the offset between every pairwise combination of
+    bricks in assembly_a and assembly_b.  However we throw a bunch of clever
+    hacks as this to make this much more manageable.  The worst case is
+    probably still N^2, but this should only come up in pathological cases.
+    
+    This is optimized for the case where assembly_b is larger than assembly_a.
     '''
     
     # Build the kdtree if one was not passed in.
     if kdtree is None:
-        kdtree = cKDTree(config_b['pose'][:,:3,3])
+        kdtree = cKDTree(assembly_b['pose'][:,:3,3])
     
     # Initialize the set of matches that have been tested already.
     ab_tested_matches = set()
     
-    # Filter the poses in a to only contain non-zero entries
-    
     # Order the classes from least common to common.
     # This makes it more likely that we will find a good match sooner.
-    unique_a, count_a = numpy.unique(config_a['class'], return_counts=True)
+    unique_a, count_a = numpy.unique(assembly_a['class'], return_counts=True)
     sort_order = numpy.argsort(count_a)
     class_order = unique_a[sort_order]
     
@@ -47,14 +52,14 @@ def match_configurations(
             if c == 0:
                 continue
             
-            instance_indices_b = numpy.where(config_b['class'] == c)[0]
+            instance_indices_b = numpy.where(assembly_b['class'] == c)[0]
             if not len(instance_indices_b):
                 continue
             
-            instance_indices_a = numpy.where(config_a['class'] == c)[0]
+            instance_indices_a = numpy.where(assembly_a['class'] == c)[0]
             
             for a in instance_indices_a:
-                color_a = config_a['color'][a]
+                color_a = assembly_a['color'][a]
                 for b in instance_indices_b:
                     # If a and b are matched under the current best offset
                     # even if they are not matched to each other, don't
@@ -68,28 +73,24 @@ def match_configurations(
                         continue
                     
                     # If the colors don't match, do not consider this offset.
-                    color_b = config_b['color'][b]
+                    color_b = assembly_b['color'][b]
                     if color_a != color_b:
                         continue
                     
                     # Compute the offset between a and b.
-                    pose_a = config_a['pose'][a]
-                    pose_b = config_b['pose'][b]
+                    pose_a = assembly_a['pose'][a]
+                    pose_b = assembly_b['pose'][b]
                     a_to_b = pose_b @ numpy.linalg.inv(pose_a)
-                    transformed_a = numpy.matmul(a_to_b, config_a['pose'])
-                    #transformed_a = numpy.matmul(a_to_b, sparse_poses_a)
+                    transformed_a = numpy.matmul(a_to_b, assembly_a['pose'])
                     
                     # Compute the closeset points.
                     pos_a = transformed_a[:,:3,3]
                     matches = kdtree.query_ball_point(pos_a, radius)
-                    #matches = [[] for _ in config_a['class']]
-                    #for i, sparse_match in zip(sparse_id_a, sparse_matches):
-                    #    matches[i] = sparse_match
                     
                     # If the number of matches is less than the current best
                     # skip the validation step.
                     potential_matches = sum(
-                        1 for c, m in zip(config_a['class'], matches)
+                        1 for c, m in zip(assembly_a['class'], matches)
                         if len(m) and c != 0
                     )
                     if potential_matches <= len(best_matches):
@@ -97,23 +98,23 @@ def match_configurations(
                     
                     # Validate the matches.
                     valid_matches = validate_matches(
-                        config_a, config_b, matches, a_to_b)
+                        assembly_a, assembly_b, matches, a_to_b)
                     
                     # Update the set of tested matches with everything that was
                     # matched in this comparison, this avoids reconsidering the
-                    # same offset later.
+                    # same offset again later.
                     ab_tested_matches.update(valid_matches)
                     
                     # If the number of valid matches is the best so far, update
                     # and break.  Breaking will exit all the way out to the
                     # main while loop and start over from the beginning.
-                    # This is important because
-                    # it allows us to reconsider offsets that we might have
-                    # skipped because of the first short-circuit in this block.
-                    # Two bricks a and b may have been connected in the previous
-                    # alignment, but may not be connected after this better
-                    # alignment, so now we need to consider them again.
-                    # As convoluted as this is, it saves a ton of computation.
+                    # This is important because it allows us to reconsider
+                    # offsets that we might have skipped because of the first
+                    # short-circuit in this block.  Two bricks a and b may
+                    # have been connected in the previous alignment, but may
+                    # not be connected after this better alignment, so now we
+                    # need to consider them again.  As convoluted as this is,
+                    # it saves a ton of computation.
                     if len(valid_matches) > len(best_matches):
                         best_alignment = (a,b)
                         best_matches.clear()
@@ -137,24 +138,24 @@ def match_configurations(
     # Return.
     return best_matches, best_offset
 
-def validate_matches(config_a, config_b, matches, a_to_b):
+def validate_matches(assembly_a, assembly_b, matches, a_to_b):
     # Ensure that classes match, colors match, poses match and that each brick
     # is only matched to one other.
     valid_matches = set()
     for a, a_matches in enumerate(matches):
         for b in a_matches:
-            class_a = config_a['class'][a]
-            class_b = config_b['class'][b]
+            class_a = assembly_a['class'][a]
+            class_b = assembly_b['class'][b]
             if class_a != class_b or class_a == 0 or class_b == 0:
                 continue
             
-            color_a = config_a['color'][a]
-            color_b = config_b['color'][b]
+            color_a = assembly_a['color'][a]
+            color_b = assembly_b['color'][b]
             if color_a != color_b:
                 continue
             
-            transformed_pose_a = a_to_b @ config_a['pose'][a]
-            pose_b = config_b['pose'][b]
+            transformed_pose_a = a_to_b @ assembly_a['pose'][a]
+            pose_b = assembly_b['pose'][b]
             if not default_allclose(transformed_pose_a, pose_b):
                 continue
             
@@ -163,12 +164,12 @@ def validate_matches(config_a, config_b, matches, a_to_b):
     
     return valid_matches
 
-def match_lookup(matching, a_config, b_config):
+def match_lookup(matching, assembly_a, assembly_b):
     a_to_b = {a:b for a, b in matching}
     b_to_a = {b:a for a, b in matching}
-    a_instances = numpy.where(a_config['class'] != 0)[0]
+    a_instances = numpy.where(assembly_a['class'] != 0)[0]
     miss_a = set(a for a in a_instances if a not in a_to_b)
-    b_instances = numpy.where(b_config['class'] != 0)[0]
+    b_instances = numpy.where(assembly_b['class'] != 0)[0]
     miss_b = set(b for b in b_instances if b not in b_to_a)
     
     return a_to_b, b_to_a, miss_a, miss_b
