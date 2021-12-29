368ef6d25ee7e9cedeec1f687dcb5a5ae4e91408
Aaron Walsman <aaronwalsman@gmail.com>
2021-12-29 14:30:31 -0800

    adding tile deduplication component

---
+++ ltron/gym/components/tile.py
@@ -0,0 +1,44 @@
+from ltron.gym.components.ltron_gym_component import LtronGymComponent
+from ltron.gym.spaces import SegmentationSpace
+
+class DeduplicateTileMaskComponent(LtronGymComponent):
+    def __init__(self,
+        tile_width,
+        tile_height,
+        render_component,
+        background=102,
+    ):
+        self.tile_width = tile_width
+        self.tile_height = tile_height
+        self.render_component = render_component
+        self.step_component = step_component
+        
+        assert self.render_component.width % self.tile_width == 0
+        assert self.render_component.height % self.tile_height == 0
+        self.width = self.render_component.width // self.tile_width
+        self.height = self.render_component.height // self.tile_height
+        
+        self.observation_space = SegmentationSpace(self.width, self.height, 1)
+        self.background = background
+    
+    def observe(self):
+        frame = self.render_component.observation
+        h, w, c = frame.shape
+        match = frame != self.previous_frame
+        match = match.reshape(
+            self.height, self.tile_height, self.width, self.tile_width, c)
+        match = numpy.moveaxis(match, 2, 1)
+        match = match.reshape(self.height, self.width, -1)
+        self.observation = numpy.all(match, axis=-1).reshape(
+            self.height, self.width)
+        
+        self.previous_frame = frame
+    
+    def reset(self):
+        self.previous_frame = self.background
+        self.observe()
+        return self.observation
+    
+    def step(self, action):
+        self.observe()
+        return self.observation, 0., False, None
