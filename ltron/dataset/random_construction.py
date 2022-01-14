8bfb8c3d300aed5989526dddbdc40a732fd0ea3e
Aaron Walsman <aaronwalsman@gmail.com>
2022-01-13 19:54:14 -0800

    more test

--- ltron/dataset/random_construction.py
+++ ltron/dataset/random_construction.py
@@ -21,33 +21,41 @@ colors_c006 = ['1','4','7','14','22','25']
 
 
 def make_mpd(
-    collection, name, samplers, colors, num_scenes, min_bricks, max_bricks):
+    collection,
+    name,
+    samplers,
+    colors,
+    start_scene,
+    num_scenes,
+    min_bricks,
+    max_bricks
+):
     ldraw_path = os.path.join(settings.collections[collection], 'ldraw')
     if not os.path.isdir(ldraw_path):
         os.makedirs(ldraw_path)
     
     scene = BrickScene(
         renderable=True, track_snaps=True, collision_checker=True)
-    for i in tqdm.tqdm(range(num_scenes)):
+    for i in tqdm.tqdm(range(start_scene, num_scenes+start_scene)):
         sample_scene(
-            scene, samplers, (min_bricks, max_bricks), colors, timeout=10, debug=True)
+            scene, samplers, (min_bricks, max_bricks), colors, timeout=10)
         scene_path = os.path.join(ldraw_path, '%s_%06i.mpd'%(name, i))
         scene.export_ldraw(scene_path)
         
         scene.clear_instances()
-        exit()
 
-def make_scale(collection, scale, num_scenes):
+def make_scale(collection, scale, start_scene, num_scenes):
     num_bricks = getattr(scales, '%s_max_bricks'%scale)
     make_mpd(
         collection,
         scale,
         samplers_s006,
         colors_c006,
-        50000,
+        start_scene,
+        num_scenes,
         num_bricks,
         num_bricks,
     )
 
-make_scale('random_construction_6_6', 'micro', 50000)
-#make_scale('random_construction_6_6', 'micro', 50000)
+#make_scale('random_construction_6_6', 'pico', 0, 50000)
+make_scale('random_construction_6_6', 'pico', 50000, 5000)
