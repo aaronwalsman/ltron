ec0c923047c16638a2f53e53e79063f650ab4e30
Aaron Walsman <aaronwalsman@gmail.com>
2022-05-02 21:53:04 -0700

    probably remove this build expert

---
+++ ltron/constants.py
@@ -0,0 +1,6 @@
+# revisit this once we have all snaps accounted for
+MAX_SNAPS_PER_BRICK = 4096
+
+# default bounds on the scene in LDU
+# 20 LDU = 1 stud + brick width, so 100000 = 5000 studs
+WORLD_BBOX = ((-100000,-100000,-100000),(100000,100000,100000))
