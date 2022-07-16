d2483bba0fb23fa8e8fc649dea324964c9dd6988
Aaron Walsman <aaronwalsman@gmail.com>
2022-07-16 14:06:59 -0400

    cleaning/pruning ECCV v_1 release

--- ltron/blender/test_run.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('/media/awalsman/LEGODrive/ltron')
-sys.path.append('/home/awalsman/Development/splendor-render')
-
-import ltron.blender.export_obj as export_obj
-export_obj.export_brick('3006.dat', directory='/home/awalsman/Documents/tmp', overwrite=True)
