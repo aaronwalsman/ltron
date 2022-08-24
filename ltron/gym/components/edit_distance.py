e29c7bae98673a9f311722683c0cf8643a7cddd5
Aaron Walsman <aaronwalsman@gmail.com>
2022-08-23 20:33:00 -0400

    adding miss_a_penalty and pose_penalty

--- ltron/gym/components/edit_distance.py
+++ ltron/gym/components/edit_distance.py
@@ -2,16 +2,21 @@ from ltron.score import edit_distance
 
 from ltron.gym.components.ltron_gym_component import LtronGymComponent
 
-
 class EditDistance(LtronGymComponent):
     def __init__(self,
         initial_assembly_component,
         current_assembly_component,
         shape_ids,
+        miss_a_penalty=1,
+        miss_b_penalty=2,
+        pose_penalty=1,
     ):
         self.initial_assembly_component = initial_assembly_component
         self.current_assembly_component = current_assembly_component
         self.part_names = {value:key for key, value in shape_ids.items()}
+        self.miss_a_penalty = miss_a_penalty
+        self.miss_b_penalty = miss_b_penalty
+        self.pose_penalty = pose_penalty
 
     def observe(self):
         initial_assembly = self.initial_assembly_component.observe()
@@ -21,7 +26,9 @@ class EditDistance(LtronGymComponent):
             current_assembly,
             initial_assembly,
             self.part_names,
-            miss_b_penalty=2,
+            miss_a_penalty=self.miss_a_penalty,
+            miss_b_penalty=self.miss_b_penalty,
+            pose_penalty=self.pose_penalty
         )
 
     def reset(self):
@@ -35,4 +42,3 @@ class EditDistance(LtronGymComponent):
     def set_state(self, state):
         self.observe()
         return None
-
