ff2cd67ab01d5b31d99a7ee7bdad1d4693e68083
Aaron Walsman <aaronwalsman@gmail.com>
2023-06-10 21:40:01 -0700

    removing old false starts

--- ltron/eval2.py
+++
@@ -1,8 +0,0 @@
-from ltron import matching
-
-def edit_distance(assembly_a, assembly_b, part_names):
-    first_matches, first_offset = matching.match_assemblies(
-        assembly_a, assembly_b, part_names)
-    
-    import pdb
-    pdb.set_trace()
